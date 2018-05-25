e64c6840b02c7bd2de177fec69584af225714817
Liam Cavens <liamcavens@googlemail.com>
2018-05-25 11:28:51 +0100

    added console class

---
+++ src/main/java/models/Stock/Console.java
@@ -0,0 +1,29 @@
+package models.Stock;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "consoles")
+public class Console extends Stock {
+
+    private ConsoleSystem system;
+
+    public Console(String name, String description, int quantity, double boughtInPrice, double sellPrice, ConsoleSystem system) {
+        super(name, description, quantity, boughtInPrice, sellPrice);
+        this.system = system;
+    }
+
+    public Console() {
+    }
+
+    @Column(name = "system")
+    public ConsoleSystem getSystem() {
+        return system;
+    }
+
+    public void setSystem(ConsoleSystem system) {
+        this.system = system;
+    }
+}
---
+++ src/main/java/models/Stock/ConsoleSystem.java
@@ -0,0 +1,10 @@
+package models.Stock;
+
+public enum ConsoleSystem {
+
+    ATARI,
+    NINTENDOSWITCH,
+    PC,
+    PLAYSTATION4,
+    XBOXONE;
+}
