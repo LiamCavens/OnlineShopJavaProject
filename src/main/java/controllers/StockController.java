df7ac0e5ec56f02a370dda57af6d33b9b98ee9a8
Scott Stevenson <scotifer92@googlemail.com>
2018-05-25 15:12:42 +0100

    Adds stock controller

--- src/main/java/controllers/StockController.java
+++ src/main/java/controllers/StockController.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import db.DBHelper;
+import models.Stock.Stock;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class StockController {
+
+    public StockController(){
+        this.setupEndpoints();
+    }
+
+    private void setupEndpoints() {
+
+        get("/stock", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Stock> stock = DBHelper.getAll(Stock.class);
+            model.put("stock", stock);
+            model.put("template", "templates/stock/index.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
 }
