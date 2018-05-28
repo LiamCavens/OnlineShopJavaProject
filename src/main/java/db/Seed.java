8899f7d4d98d1e9ccadd8802cf5e52ab6ee58d22
Liam Cavens <liamcavens@googlemail.com>
2018-05-28 09:44:50 +0100

    host not working

--- src/main/java/db/Seed.java
+++ src/main/java/db/Seed.java
@@ -10,7 +10,7 @@ public class Seed {
 
 //        DBHelper.deleteAll(Basket.class);
 //        DBHelper.deleteAll(Customer.class);
-       DBHelper.deleteAll(Clothing.class);
+//       DBHelper.deleteAll(Clothing.class);
        DBHelper.deleteAll(Game.class);
 //        DBHelper.deleteAll(stock.class);
 
