7128a0b8d8f9a862da652239e85b6c28427d9bda
Scott Stevenson <scotifer92@googlemail.com>
2018-05-28 14:12:56 +0100

    Clean MasterController

--- src/main/java/db/Seed.java
+++ src/main/java/db/Seed.java
@@ -1,22 +1,22 @@
 package db;
 
 import models.Basket;
+import models.Customer;
 import models.stock.Clothing;
 import models.stock.Console;
 import models.stock.Game;
+import models.stock.Stock;
 
 public class Seed {
 
     public static void seedData(){
 
         DBHelper.deleteAll(Basket.class);
-//        DBHelper.deleteAll(Customer.class);
-       DBHelper.deleteAll(Clothing.class);
-       DBHelper.deleteAll(Game.class);
-//        DBHelper.deleteAll(stock.class);
+        DBHelper.deleteAll(Customer.class);
+        DBHelper.deleteAll(Clothing.class);
+        DBHelper.deleteAll(Game.class);
+        DBHelper.deleteAll(Stock.class);
 
-
-        Basket defaultBasket = new Basket();
 
         Game game1 = new Game("Grand Theft Auto V", "Grand Theft Auto V (also known as Grand Theft Auto Five, GTA 5 or GTA V) is a video game developed by Rockstar North. It is the fifteenth instalment in the Grand Theft Auto series and the successor of Grand Theft Auto IV.", 1, 25, 45, "Action", "GHSR-4432", "https://static-cdn.jtvnw.net/ttv-boxart/Grand%20Theft%20Auto%20V.jpg", "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/hvoD7ehZPcM?rel=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>");
         Game game2 = new Game("Metal Gear Solid", "Metal Gear Solid elaborates on the storyline of the earlier games and reveals that Solid Snake is a genetic clone of Big Boss, created as part of a secret government project.", 1, 20, 40, "Strategy", "5432-DLAS", "https://www.cdkeys.com/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/e/metal_gear_solid_v_definitive_experience_pc_cover.jpg", "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/_V_86OQViQE?rel=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>");
@@ -41,11 +41,6 @@ public class Seed {
 
         DBHelper.saveOrUpdate(console1);
         DBHelper.saveOrUpdate(console2);
-
-        Basket basket = new Basket();
-        basket.addItemToBasket(game1);
-
-        DBHelper.saveOrUpdate(basket);
 
     }
 
